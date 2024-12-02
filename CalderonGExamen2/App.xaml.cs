07e8be837ad358eccb5a18c97983bb76610728dc
G4B0CM <Gabriel.calderon.matute@udla.edu.ec>
2024-12-02 07:14:39 -0500

    Agregar archivos de proyecto.

---
+++ CalderonGExamen2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CalderonGExamen2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

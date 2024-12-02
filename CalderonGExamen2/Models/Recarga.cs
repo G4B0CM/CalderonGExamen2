daadf6e848e2d5e0bd3ce87339225aa5ee270d6f
G4B0CM <Gabriel.calderon.matute@udla.edu.ec>
2024-12-02 07:43:10 -0500

    Modelo y content page terminados

---
+++ CalderonGExamen2/Models/Recarga.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalderonGExamen2.Models
+{
+    public class Recarga
+    {
+        public string Filename { get; set; }
+        public string telefono { get; set; }
+        public string nombre { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
--- CalderonGExamen2/Recarga.xaml.cs
+++ CalderonGExamen2/Recarga.xaml.cs
@@ -1,9 +1,51 @@
 namespace CalderonGExamen2;
-
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class Recarga : ContentPage
 {
-	public Recarga()
+    public string ItemId
+    {
+        set { CargarRecargas(value); }
+    }
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "GabrielCalderon.txt");
+    public Recarga()
 	{
 		InitializeComponent();
-	}
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.GabrielCalderon.txt";
+
+        CargarRecargas(Path.Combine(appDataPath, randomFileName));
+    }
+    private async void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.Recarga recarga)
+            File.WriteAllText(recarga.Filename, TextEditor.Text);
+
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.Recarga recarga)
+        {
+            // Delete the file.
+            if (File.Exists(recarga.Filename))
+                File.Delete(recarga.Filename);
+        }
+
+        await Shell.Current.GoToAsync("..");
+    }
+    private void CargarRecargas(string fileName)
+    {
+        Models.Recarga recargaModelo = new Models.Recarga();
+        recargaModelo.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            recargaModelo.Date = File.GetCreationTime(fileName);
+            recargaModelo.telefono = File.ReadAllText(fileName);
+            recargaModelo.nombre = File.ReadAllText(fileName);
+        }
+
+        BindingContext = recargaModelo;
+    }
 }
